ea85374d7c6045473af2b157eaa9561e1fce51be
cazterk <zcephas2@gmail.com>
2022-10-05 16:27:22 +0200

    implement toggle functionality for nav

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,12 @@
-console.log('HELLO');
+const navigationMenu = document.querySelector(".navbar__menu");
+const navToggle = document.querySelector(".navbar__bars");
 
-const test = () => {
-	console.log('this is a test');
-};
+navToggle.addEventListener("click", () => {
+  const visibility = navigationMenu.getAttribute("data-visible");
+  console.log(visibility);
+  return visibility === "false"
+    ? navigationMenu.setAttribute("data-visible", true)
+    : navigationMenu.setAttribute("data-visible", false);
+
+  //   return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+});
