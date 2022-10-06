bd902e42b92f27397fa42d1bbb138e6b5d8c675b
cazterk <zcephas2@gmail.com>
2022-10-06 14:55:12 +0200

    add animations && transitions to navbar

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,29 @@
 const navigationMenu = document.querySelector(".navbar__menu");
 const navToggle = document.querySelector(".navbar__bars");
 
-navToggle.addEventListener("click", () => {
-  const visibility = navigationMenu.getAttribute("data-visible");
-  console.log(visibility);
-  return visibility === "false"
-    ? navigationMenu.setAttribute("data-visible", true)
-    : navigationMenu.setAttribute("data-visible", false);
+let resizeTimer;
 
-  //   return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
-});
+const toggleAcions = () => {
+  const visibility = navigationMenu.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    navigationMenu.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+    return (navToggle.innerHTML = `<i class="fa-solid fa-xmark" ></i>`);
+  } else {
+    navigationMenu.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+    return (navToggle.innerHTML = `<i class="fa-solid fa-bars"></i>`);
+  }
+};
+
+const preventUnwantedTransitions = () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+};
+
+navToggle.addEventListener("click", toggleAcions);
+window.addEventListener("resize", preventUnwantedTransitions);
